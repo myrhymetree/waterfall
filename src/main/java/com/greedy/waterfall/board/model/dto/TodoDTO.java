dc1bf92fec3f982889b84b947cab296225fd9bc1
Hwa-eung Cha <hwaeung@gmail.com>
2022-02-19 23:28:31 +0900

    FEAT : 투두 게시판 기능[추가]
    수정, 삭제, 등록, 상세페이지

--- src/main/java/com/greedy/waterfall/board/model/dto/TodoDTO.java
+++ src/main/java/com/greedy/waterfall/board/model/dto/TodoDTO.java
@@ -1,5 +1,31 @@
 package com.greedy.waterfall.board.model.dto;
 
+import java.sql.Date;
+
+import com.greedy.waterfall.member.model.dto.MemberDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor	//기본 생성자
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class TodoDTO {
 
+	private int no;
+	private Date postingDate;
+	private String title;
+	private String content;
+	private String status;
+	private int categoryNo;
+	private int count;
+	private MemberDTO writer;
+	
+//	public TodoDTO() {}	//기본 생성자
+	
 }
