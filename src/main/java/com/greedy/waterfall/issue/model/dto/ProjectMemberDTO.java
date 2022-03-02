de35278c109dda8bdda9672816f3f0e43238b03a
young.kim <zxcv4097@gmail.com>
2022-03-03 00:41:14 +0900

    FEAT :  projectDTO [추가]

---
+++ src/main/java/com/greedy/waterfall/issue/model/dto/ProjectMemberDTO.java
@@ -0,0 +1,19 @@
+package com.greedy.waterfall.issue.model.dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@Builder
+public class ProjectMemberDTO {
+
+   private int memberNo;
+   private String  memberName;
+}
