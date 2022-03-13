a61230146ce948a89c08c9c4253d7dce2bfba4b5
h99www <h99www@gmail.com>
2022-03-13 20:54:07 +0900

    !HOTFIX: 경로오류 [수정]

--- src/main/java/com/greedy/waterfall/project/controller/ProjectControllerView.java
+++ src/main/java/com/greedy/waterfall/project/controller/ProjectControllerView.java
@@ -4,6 +4,8 @@ public interface ProjectControllerView {
 
 	final static String PROJECT_REGIST_VIEW = "/project/projectRegist";
 	final static String REDIRECT_MANAGE_VIEW = "redirect:/project/managelist";
+	final static String REDIRECT_LIST_VIEW = "redirect:/project/projectList";
+	final static String PROJECT_LIST_VIEW = "/project/projectList";
 	final static String PROJECT_MANAGE_VIEW = "/project/projectManage";
 	final static String JSON_VIEW = "jsonView";
 	final static String PROJECT_MODIFY_VIEW = "/project/projectModify";  
